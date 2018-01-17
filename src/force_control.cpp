80590fcf77e9c748c1ae1a2dec918c03478fa913
Julen <judj.core@gmail.com>
2018-01-17 16:48:00 +0100

    new additions

---
+++ src/force_control.cpp
@@ -0,0 +1,19 @@
+// Eigen headers
+#include <Eigen/Core>
+
+// iDynTree headers
+#include <iDynTree/Model/FreeFloatingState.h>
+#include <iDynTree/KinDynComputations.h>
+#include <iDynTree/ModelIO/ModelLoader.h>
+#include <iDynTree/Model/FreeFloatingState.h>
+#include <iDynTree/Model/FreeFloatingMatrices.h>
+
+// Helpers function to convert between
+// iDynTree datastructures
+#include <iDynTree/Core/EigenHelpers.h>
+#include <math.h>
+
+
+
+
+
